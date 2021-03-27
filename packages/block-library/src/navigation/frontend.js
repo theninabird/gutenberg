7c46e01b0fa8611d9537fb3298397733cddf83c5
jasmussen <joen@automattic.com>
2021-03-27 11:47:03 +0100

    Try: add has-modal-open class on html element

--- packages/block-library/src/navigation/frontend.js
+++ packages/block-library/src/navigation/frontend.js
@@ -3,6 +3,15 @@
  */
 import MicroModal from 'micromodal';
 
+function navigationToggleModal() {
+	const toggleClass = ( el, className ) => el.classList.toggle( className );
+	toggleClass( document.querySelector( 'html' ), 'has-modal-open' );
+}
+
 MicroModal.init( {
+	// eslint-disable-next-line no-unused-vars
+	onShow: ( modal ) => navigationToggleModal(),
+	// eslint-disable-next-line no-unused-vars
+	onClose: ( modal ) => navigationToggleModal(),
 	openClass: 'is-menu-open',
 } );
